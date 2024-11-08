14267fce795524852e89fc7648d2380963c4ee20
xmartina <martinsprince126@gmail.com>
2024-11-08 03:51:22 +0100

    d

--- index.php
+++ index.php
@@ -227,7 +227,9 @@ include_once __DIR__.'/partials/header.php';
     <div class="status">
         <p>Status: <span id="statusMessage">Awaiting Payment</span></p>
     </div>
-    <div class="btn btn-warning px-3 py-3">Payment Completed</div>
+    <div class="d-flex justify-content-center">
+        <div class="btn btn-primary px-3 py-2">Payment Completed</div>
+    </div>
 
     <!-- Footer -->
     <div class="footer">
